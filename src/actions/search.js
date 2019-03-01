0e1d68764eecc3f10928884c7ec7d597b0de4962
Bruce <bruce@yogainternational.com>
2019-02-28 21:40:17 -0500

    Complete defining action creators

--- src/actions/search.js
+++ src/actions/search.js
@@ -1,22 +1,41 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
 import searchYouTube from '../lib/searchYouTube.js';
 import changeVideoList from './videoList.js';
 import changeVideo from './currentVideo.js';
 import YOUTUBE_API_KEY from '../config/youtube.js';
-import store from '../store/store.js'
-
 
 var handleVideoSearch = (q) => {
-
   //TODO:  Write an asynchronous action to handle a video search!
-  return (dispatch) => {
-    dispatch(searchYouTube({
-      key: YOUTUBE_API_KEY,
-      query: q
-    }));
-  }
+  return () => {
+    return searchYouTube(
+      {
+        key: YOUTUBE_API_KEY,
+        query: q,
+      },
+      (data) => {
+        changeVideoList(data);
+        changeVideo(data[0]);
+      }
+    );
+  };
 };
+
+// var handleVideoSearch = (q) => {
+//   //TODO:  Write an asynchronous action to handle a video search!
+//   return (dispatch) => {
+//     searchYouTube(
+//       {
+//         key: YOUTUBE_API_KEY,
+//         query: q,
+//       },
+//       (data) => data
+//     ).then((result) => {
+//       dispatch({
+//         type: 'SEARCH',
+//         videos: result,
+//       });
+//     });
+//   };
+// };
 
 export default handleVideoSearch;
 
